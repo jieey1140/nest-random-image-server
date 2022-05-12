9f0e7083eb362258edefe4563024389320fa0bbe
jieey1140 <seihoonkim@hansei.kr>
2022-05-12 20:35:48 +0900

    feat random image one add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('images')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('image/')
+  getHello(@Res() response: Response) {
+    return this.appService.getRandomImagesOneResponse(response);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
+import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
 import { Injectable } from '@nestjs/common';
-
+import { ImageEntity } from './images/image.init';
+import { readFileSync } from 'fs';
+import * as _ from 'lodash';
+import { Response } from 'express';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly imagesInMemoryService: InMemoryDBService<ImageEntity>,
+  ) {}
+  public async getRandomImagesOneResponse(res: Response) {
+    const getImageAll = this.imagesInMemoryService.getAll().map((item) => {
+      return item.item;
+    });
+    const data = readFileSync('public/images/' + _.sample(getImageAll));
+    return res.set({ 'Content-Type': 'image/jpeg' }).send(data);
   }
 }
